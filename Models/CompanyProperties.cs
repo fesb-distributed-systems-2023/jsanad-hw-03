07ff25505c18e85afaca8f876be0360124589337
Josip1201 <jsanad00@fesb.hr>
2023-11-17 10:37:05 +0100

    Uploading model

---
+++ Models/CompanyProperties.cs
@@ -0,0 +1,11 @@
+﻿namespace DIS_projekt.Models
+{
+    public class CompanyProperties
+    {
+        public int Id { get; set; }
+        public string CompanyName { get; set; }
+        public string EmployeeName { get; set; }
+        public string Product { get; set; }
+        public int Revenue { get; set; }
+    }
+}
