1642db14be25b135707fa613b70c6e6b7896323c
Josip1201 <jsanad00@fesb.hr>
2023-11-20 11:15:43 +0100

    Ispravljeno

--- Models/CompanyProperties.cs
+++ Models/CompanyProperties.cs
@@ -1,10 +1,10 @@
 ﻿namespace DIS_projekt.Models
 {
-    public class CompanyProperties
+    public class Company
     {
         public int Id { get; set; }
         public string CompanyName { get; set; }
-        public string EmployeeName { get; set; }
+        public string OwnerName { get; set; }
         public string Product { get; set; }
         public int Revenue { get; set; }
     }
